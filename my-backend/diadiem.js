42dd6b6faf1d30305f8bcf8419107fbcfb956e0b
khuongn2k3 <khuongn2k3@gmail.com>
2025-07-15 18:03:33 +0700

    Add files via upload

---
+++ my-backend/diadiem.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const destinationSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  image: String,
+  url: String
+});
+
+module.exports = mongoose.model('Destination', destinationSchema, 'diadiem');
