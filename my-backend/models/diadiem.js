9c3f23d2fc06cd04f9d16ca855e0f2738941cde4
khuongn2k3 <khuongn2k3@gmail.com>
2025-07-15 18:07:40 +0700

    Update diadiem.js

--- my-backend/models/diadiem.js
+++ my-backend/models/diadiem.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const destinationSchema = new mongoose.Schema({
+const diaDiemSchema = new mongoose.Schema({
   title: String,
   description: String,
   image: String,
   url: String
 });
 
-module.exports = mongoose.model('Destination', destinationSchema, 'diadiem');
+module.exports = mongoose.model('diadiem', diaDiemSchema);
