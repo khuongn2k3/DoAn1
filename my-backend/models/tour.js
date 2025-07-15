42a6e62082124713cfe1d48a648eb238237fe8be
khuongn2k3 <khuongn2k3@gmail.com>
2025-07-15 19:31:03 +0700

    Update tour.js

--- my-backend/models/tour.js
+++ my-backend/models/tour.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
 
+const tourSchema = new mongoose.Schema({
+  tenTour: String,
+  gia: Number,
+  ngayKhoiHanh: Date,
+  soNgay: Number,
+  diemKhoiHanh: String,        
+  moTa: String,
+  hinhAnh: String,
+});
+
+module.exports = mongoose.model('tour', tourSchema);
