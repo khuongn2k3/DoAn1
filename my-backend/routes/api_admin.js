e5327438f53e313346bb7381ad1cce8187f02961
khuongn2k3 <khuongn2k3@gmail.com>
2025-07-17 22:58:07 +0700

    Create api_admin.js

---
+++ my-backend/routes/api_admin.js
@@ -0,0 +1,8 @@
+router.post('/login', async (req, res) => {
+  const { email, matKhau } = req.body;
+  const admin = await Admin.findOne({ email, matKhau });
+
+  if (!admin) return res.status(401).json({ message: 'Sai tài khoản hoặc mật khẩu' });
+
+  res.json({ _id: admin._id, hoTen: admin.hoTen });
+});
