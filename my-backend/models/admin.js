3689383efadc5f784c5bde67a6c2eeea853c4de0
khuongn2k3 <khuongn2k3@gmail.com>
2025-07-17 23:21:27 +0700

    Update admin.js

--- my-backend/models/admin.js
+++ my-backend/models/admin.js
@@ -4,7 +4,7 @@ const adminSchema = new mongoose.Schema({
   hoTen: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   matKhau: { type: String, required: true },
-  avatar: { type: String, default: '' },
+  avatar: { type: String, default: 'admin1.jpg' },
   ngayTao: { type: Date, default: Date.now }
 });
 
